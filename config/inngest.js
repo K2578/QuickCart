15750e16cff37a7854fc20454cad5d413ef4bec1
Muhammad Ayman <muhaymanham@gmail.com>
2025-11-24 10:28:43 +0700

    fourth commit

--- config/inngest.js
+++ config/inngest.js
@@ -13,10 +13,10 @@ export const syncUserCreation = inngest.createFunction(
         event: 'clerk/user.created'
     },
     async ({ event }) => {
-        const { id, first_name, last_name, email, imageUrl } = event.data;
+        const { id, first_name, last_name, email_addresses, imageUrl } = event.data;
         const userData = {
             _id: id,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + " " + last_name,
             imageUrl: imageUrl
         }
